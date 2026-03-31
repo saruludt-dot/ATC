158e830f96757bbc8d545858212f411fdc933235
saruludt-dot <sarul.udt@gmail.com>
2026-03-31 21:57:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def check_login():
         password = st.text_input("Password", type="password")
 
         if st.button("Login"):
-            if username == "admin" and password == "1234":
+            if username == "ATC" and password == "2015":
                 st.session_state.logged_in = True
                 st.success("Login successful")
                 st.rerun()
