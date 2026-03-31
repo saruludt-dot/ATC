34998e610d00cc86a76e9da8f96e02769c8bd013
saruludt-dot <sarul.udt@gmail.com>
2026-03-31 23:26:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -154,10 +154,10 @@ if uploaded_file and calculate:
     if diff_list:
         atm_strike, atm_ce, atm_pe, atm_diff = min(diff_list, key=lambda x: x[3])
 
-        if show_atm:
-            diff = round(atm_ce - atm_pe, 2)
-            st.subheader("📍 Minimum Difference Strike (ATM)")
-            st.success(f"Strike: {int(atm_strike)} | CE: {atm_ce:.2f} | PE: {atm_pe:.2f} | Diff: {diff:.2f}")
+        #if show_atm:
+            #diff = round(atm_ce - atm_pe, 2)
+            #st.subheader("📍 Minimum Difference Strike (ATM)")
+            #st.success(f"Strike: {int(atm_strike)} | CE: {atm_ce:.2f} | PE: {atm_pe:.2f} | Diff: {diff:.2f}")
 
         #st.divider()
 
