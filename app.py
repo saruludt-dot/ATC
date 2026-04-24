1a105f947d5a84ce648d6792a152e261f355a111
saruludt-dot <sarul.udt@gmail.com>
2026-04-25 00:08:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,13 @@ import pandas as pd
 import base64
 import streamlit.components.v1 as components
 
+import time
+
+if st.session_state.show_success:
+    st.success("✅ Calculated")
+    time.sleep(2)
+    st.session_state.show_success = False
+
 # ---------------- IMAGE ----------------
 def get_img(path):
     with open(path, "rb") as f:
